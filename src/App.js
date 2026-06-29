fce56bb33bfe386af75284ea011ce582b5d35b02
Mariam Ahmed <mariamff11@gmail.com>
2026-06-29 01:41:43 -0230

    Added Square and Board components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
-export default function Square() {
-    return <button className="square">X</button>
+// export: makes this function accessible outside of this file
+// default: tells other files using your code that this is the main function in your file
+// return: whatever is after return is returned to the caller as a value
+// <button> is a JSX element
+// A JSX element is a combination of JavaScript code and HTML tags that describes
+//      what you'd like to display
+// className is a button property or prop that tells CSS how to style the element
+// React components need to return only a single JSX element; we can use Fragments
+//      (<> and </>) for this
+// Component names must start with a capital letter, unlike HTML element names like div
+// Use curly braces to escape from JSX into JavaScript
+
+function Square({ value }) {
+    return <button className="square">{value}</button>;
+}
+
+// Square is a child of Board
+export default function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <Square value="1" />
+                <Square value="2" />
+                <Square value="3" />
+            </div>
+            <div className="board-row">
+                <Square value="4" />
+                <Square value="5" />
+                <Square value="6" />
+            </div>
+            <div className="board-row">
+                <Square value="7" />
+                <Square value="8" />
+                <Square value="9" />
+            </div>
+        </>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import {StrictMode} from "react";
-import {createRoot} from "react-dom/client";
-import "./styles.css";
+import {createRoot} from "react-dom/client";  // the library that talks to web browsers
+import "./styles.css";  // styles for the components
 
 import App from "./App";
 
+// The final product is injected into public/index.html
 const root = createRoot(document.getElementById("root"));
 root.render(
     <StrictMode>
